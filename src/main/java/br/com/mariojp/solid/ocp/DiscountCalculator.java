32fd32ecf621c536b420c8e6cb87c6c99d1df9ec
Marcelo Barros <marcelobarrossilvafilho@gmail.com>
2025-08-28 20:58:09 -0300

    refactor: modifica DiscountCalculator.java para usar DiscountFactory.java

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -2,10 +2,12 @@ package br.com.mariojp.solid.ocp;
 
 public class DiscountCalculator {
     public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
+
+        DiscountPolicy discount = DiscountFactory.create(amount, type);
+        if (type != null){
+            return discount.apply(amount);
         }
+        return amount;
+
     }
 }
